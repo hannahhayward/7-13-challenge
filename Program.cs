6bc28220c70411735aff59a38c99514039d0a9b7
hannah hayward <hannahhayward97@gmail.com>
2021-07-13 10:20:48 -0600

    first attempt

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7_13_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Dictionary<string, string> users = new Dictionary<string, string>();
+            Console.WriteLine("enter your username");
+            var name = Console.ReadLine();
+            Console.WriteLine("your username is" + name);
+            Console.WriteLine(" enter your password ");
+            var password = Console.ReadLine();
+            Console.WriteLine("your password is" + password) ;
+            users.Add(name, password );
+            foreach(KeyValuePair<string, string> user in users)
+            {
+                Console.WriteLine(name, password);
+            }
+        }
+            }
+}
